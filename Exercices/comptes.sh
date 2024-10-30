22c4ce3794b03300d7c8e0dd1eb7b8e587485383
Catalin Ciomne <catalin.ciomne@sorbonne-nouvelle.fr>
2024-10-30 22:24:30 +0100

    remplacement du chemin absolu par un chemin relatif dans le script comptes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -3,5 +3,5 @@
 for ELEMENT in 2016 2017 2018
 do
 	echo "Annotations de lieux en $ELEMENT :"
-	grep -E "Location" ~/Cours/2024\ -\ 2025/Semestre\ 1/Programmation\ et\ projet\ encadré/Seance\ 1/Exercice1/ann/$ELEMENT/*.ann | wc -l
+	grep -E "Location" $ELEMENT/*.ann | wc -l
 done
